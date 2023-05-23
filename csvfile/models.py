833799796ea2f8ae61696d51173139ec368ca4b6
himanshuvb <savvysmith001@gmail.com>
2023-05-23 23:46:41 +0530

    commitgetintouch

--- csvfile/models.py
+++ csvfile/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class ContactForm(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    message = models.TextField()
+
+    def __str__(self):
+        return self.name
